00bd1c043978a0b8562e7d15ce2465bbb5deb960
Kamyameha <grascam@gmail.com>
2026-07-17 17:11:50 +0300

    Bump cache for UI refinements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='entre-deux-qualiopi-secours-v7';
+const CACHE='entre-deux-qualiopi-secours-v8';
 const SECOURS=['./','./index.html','./css/main.css','./css/site-alignment.css','./css/audit-ui.css','./js/app.js','./js/version.js','./data/qualiopi.json','./assets/logo-entre-deux.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SECOURS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
